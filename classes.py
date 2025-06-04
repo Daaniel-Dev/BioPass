f480c53579ccafdda60e26a00383c954a514cc92
Daniel <83424429+Daaniel-Dev@users.noreply.github.com>
2025-06-04 18:34:31 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -3,23 +3,23 @@ class User:
         self.cpf = cpf
         self.senha = senha
         self.nome = nome
-    def chamar_dados(self):
-        return self.cpf, self.senha, self.nome, 'Cliente'
-
-
-class Vendedor(User):
-    def __init__(self, cpf, senha, nome):
-        super().__init__(cpf, senha, nome)
-        self.produtos = []
-    def chamar_dados(self):
-        return self.cpf, self.senha, self.nome, 'Vendedor'
+    def dados(self):
+        return {"cpf": self.cpf, "senha": self.senha, "nome": self.nome}
 
 
 class Produto:
-    def __init__(self, nome, preco, descricao, url_video=''):
-        self.nome = nome
+    def __init__(self, id_vendedor, nome_vendedor, nome_produto, preco, quantidade, descricao, url_video=''):
+        self.id_vendedor = id_vendedor
+        self.nome_vendedor = nome_vendedor
+        self.nome_produto = nome_produto
         self.preco = preco
         self.descricao = descricao
+        self.quantidade = quantidade
+        self.url_video = url_video
+    def dados(self):
+        return {"id": self.id_vendedor, "nome do vendedor": self.nome_vendedor,
+                "nome do produto": self.nome_produto, "preço": self.preco,
+                "descrição": self.descricao, "quantidade": self.quantidade, "url": self.url_video}
         self.url_video = url_video
 
     def __str__(self):
