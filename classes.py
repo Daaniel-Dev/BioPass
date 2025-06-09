b9c9144ebfdbbad2d753d092d41cbcb6565572df
Daniel <83424429+Daaniel-Dev@users.noreply.github.com>
2025-06-09 18:43:06 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -8,16 +8,16 @@ class User:
 
 
 class Produto:
-    def __init__(self, id_vendedor, nome_vendedor, nome_produto, preco, quantidade, descricao, url_video=''):
-        self.id_vendedor = id_vendedor
+    def __init__(self, cpf_vendedor, nome_vendedor, id_produto, nome_produto, preco, quantidade, descricao, url_video=''):
+        self.cpf_vendedor = cpf_vendedor
         self.nome_vendedor = nome_vendedor
+        self.id_produto = id_produto
         self.nome_produto = nome_produto
         self.preco = preco
         self.descricao = descricao
         self.quantidade = quantidade
         self.url_video = url_video
     def dados(self):
-        return {"id": self.id_vendedor, "nome do vendedor": self.nome_vendedor,
-                "nome do produto": self.nome_produto, "preço": self.preco,
-                "descrição": self.descricao, "quantidade": self.quantidade, "url": self.url_video}
-        self.url_video = url_video
+        return {"cpf": self.cpf_vendedor, "id": self.id_produto, "nome do vendedor": self.nome_vendedor,
+                "nome do produto": self.nome_produto, "preço": self.preco, "quantidade": self.quantidade,
+                "descrição": self.descricao, "url": self.url_video}
