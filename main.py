3b44d03aa511e4e29cdca9667ba6da383b9ab72c
Daniel <83424429+Daaniel-Dev@users.noreply.github.com>
2025-06-09 18:42:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 from functions import *
 
-espacos = 65
-titulo('Ecora - Seu marketplace de produtos sustentáveis!', espacos=espacos)
-while True:
-    opcao1 = menu(['Login', 'Cadastrar', 'Sair'])
-    if opcao1 == 0:
-        usuario = login_usuario()
-        if usuario == 'Voltar':
-            continue
-        titulo(f'Seja bem vindo, {usuario['nome']}!', '-', espacos)
-        while True:
-            print('-' * espacos)
-            opcao2 = menu(['Ver produtos disponíveis', 'Comprar produto', 'Cadastrar produto', 'Depoimentos',
-                      'Vídeos educacionais', 'Finalizar sessão'])
-            if opcao2 == 0:
-                listar_produtos_disponiveis()
-            elif opcao2 == 1:
-                comprar_produto()
-            elif opcao2 == 2:
-                cadastrar_produto(usuario)
-            elif opcao2 == 3:
-                funcao_depoimentos()
-            elif opcao2 == 4:
-                video_educacional()
-            elif opcao2 == 5:
-                print('Finalizando sessão...')
-                break
-    elif opcao1 == 1:
-        usuario = cadastro_usuario()
-    elif opcao1 == 2:
-        print('Obrigado por usar nosso app.')
-        break
+esp = 65
+titulo('BioPass - Seu marketplace de produtos sustentáveis!', espacos=esp)
+try:
+    while True:
+        opcao1 = menu(['Login', 'Cadastrar', 'Sair'])
+        if opcao1 == 0:
+            usuario = login_usuario()
+            if usuario == 'Voltar':
+                continue
+            titulo(f'Seja bem vindo(a), {usuario['nome']}!', '-', esp, linha2=False)
+            while True:
+                print('-' * esp)
+                opcao2 = menu(['Ver produtos disponíveis', 'Comprar produto', 'Cadastrar produto',
+                               'Alterar informações de cadastro', 'Alterar informações do seu produto' ,'Depoimentos',
+                                'Vídeos educacionais', 'Finalizar sessão'])
+                if opcao2 == 0:
+                    listar_produtos_disponiveis(usuario)
+                elif opcao2 == 1:
+                    comprar_produto(usuario)
+                elif opcao2 == 2:
+                    cadastrar_produto(usuario)
+                elif opcao2 == 3:
+                    usuario_cadastro = alterar_cadastro_usuario(usuario)
+                    if usuario == 'Conta excluída':
+                        break
+                elif opcao2 == 4:
+                    produto_cadastro = alterar_cadastro_produto(usuario)
+                    if produto_cadastro == 'Produto excluído':
+                        break
+                elif opcao2 == 5:
+                    funcao_depoimentos(usuario)
+                elif opcao2 == 6:
+                    video_educacional()
+                elif opcao2 == 7:
+                    print('Finalizando sessão...')
+                    break
+        elif opcao1 == 1:
+            usuario = cadastro_usuario()
+        elif opcao1 == 2:
+            break
+except KeyboardInterrupt:
+    print('\nInterrompendo programa...')
+print('\033[1mObrigado por usar nosso app!\033[m')
